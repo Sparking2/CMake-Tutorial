e32055063a2e0891b4cc06997a03aa56ce1bd5a1
Sparking02 <victor.cortes_marquez@proton.me>
2024-12-05 19:53:17 -0600

    Step 2 - Adding a library

--- tutorial.cxx
+++ tutorial.cxx
@@ -2,13 +2,17 @@
 #include <cmath>
 #include <iostream>
 #include <string>
+
+// TODO 5:  Include MathFunctions.h
 #include "TutorialConfig.h"
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
+    // report version
     std::cout << "Version: " << Tutorial_VERSION_MAJOR << "." << Tutorial_VERSION_MINOR << std::endl;
 
-    if (argc < 2){
+    if (argc < 2)
+    {
         std::cout << "Usage: " << argv[0] << " number" << std::endl;
         return 1;
     }
@@ -16,8 +20,10 @@ int main(int argc, char* argv[])
     // convert input to double
     const double inputValue = std::stod(argv[1]);
 
+    // TODO 6: Replace sqrt with mathfunctions::sqrt
+
     // calculate square root
     const double outputValue = sqrt(inputValue);
-    std::cout << "The square root of " << inputValue << " is " << outputValue << std:: endl;
+    std::cout << "The square root of " << inputValue << " is " << outputValue << std::endl;
     return 0;
 }
